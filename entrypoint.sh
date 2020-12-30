ba31e5954b360fb98855e195dc294caa28ee999b
Eli Trop <eli.trop@checkmarx.com>
2020-12-30 11:27:07 +0200

    view data in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 wget -c https://github.com/Checkmarx/kics/releases/latest/download/kics_1.0.0_linux_x64.tar.gz -O - | tar -xz
 
+echo $INPUT_DIRECTORY
+
+ls -la /github/workspace
+
 ./kics -p $INPUT_DIRECTORY 
