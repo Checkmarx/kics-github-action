8261f67b55bdbd8b45170800c3574e5cc98b7ead
Eli Trop <eli.trop@checkmarx.com>
2020-12-30 15:01:52 +0200

    fix format

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ DATETIME="`date '+%H:%M'`"
 
 if [ -z "$INPUT_PATH" ]
 then
-      echo "${DATETIME} - ERROR : input path can't be empty"
+      echo "${DATETIME} - ERR input path can't be empty"
       exit 1
 else
       INPUT_PARAM="-p $INPUT_PATH"
@@ -15,12 +15,12 @@ fi
 [[ ! -z "$INPUT_VERBOSE" ]] && VERBOSE_PARAM="-v"
 
 tag=`curl --silent "https://api.github.com/repos/Checkmarx/kics/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
-echo "${DATETIME} - INF : latest tag is $tag"
+echo "${DATETIME} - INF latest tag is $tag"
 version=`echo $tag | sed -r 's/^.{1}//'`
-echo "${DATETIME} - INF : version is $version"
+echo "${DATETIME} - INF version is $version"
 
-echo "${DATETIME} - INF : downloading latest kics binaries kics_${version}_linux_x64.tar.gz"
+echo "${DATETIME} - INF downloading latest kics binaries kics_${version}_linux_x64.tar.gz"
 wget -q -c "https://github.com/Checkmarx/kics/releases/download/${tag}/kics_${version}_linux_x64.tar.gz" -O - | tar -xz &>/dev/null
 
-echo "${DATETIME} - INF :about to scan directory $INPUT_PATH"
+echo "${DATETIME} - INF : about to scan directory $INPUT_PATH"
 ./kics $INPUT_PARAM $OUTPUT_PATH_PARAM $PAYLOAD_PATH_PARAM $QUERIES_PARAM $VERBOSE_PARAM
