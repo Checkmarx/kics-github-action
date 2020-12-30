8f5f8d7dde1b63d2f7f5483b726601ee9308c93c
Eli Trop <eli.trop@checkmarx.com>
2020-12-30 14:27:50 +0200

    add params to scan

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
-#!/bin/sh -l
+#!/bin/bash
+
+INPUT_PARAM="-p $INPUT_PATH"
+
+[[ ! -z "$INPUT_OUTPUT_PATH" ]] && OUTPUT_PATH_PARAM="-o $INPUT_OUTPUT_PATH"
+[[ ! -z "$INPUT_PAYLOAD_PATH" ]] && PAYLOAD_PATH_PARAM="-d $INPUT_PAYLOAD_PATH"
+[[ ! -z "$INPUT_QUERIES" ]] && QUERIES_PARAM="-q $INPUT_QUERIES"
+[[ ! -z "$INPUT_VERBOSE" ]] && VERBOSE_PARAM="-v"
 
 tag=`curl --silent "https://api.github.com/repos/Checkmarx/kics/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
 echo 'latest tag is' $tag
@@ -8,5 +15,5 @@ echo 'version is' $version
 echo "Downloading latest kics binaries"
 wget -c "https://github.com/Checkmarx/kics/releases/download/${tag}/kics_${version}_linux_x64.tar.gz" -O - | tar -xz
 
-echo "about to scan directory" $INPUT_DIRECTORY
-./kics -p $INPUT_DIRECTORY -o results.json
+echo "about to scan directory" $INPUT_PATH
+./kics $INPUT_PARAM $OUTPUT_PATH_PARAM $PAYLOAD_PATH_PARAM $QUERIES_PARAM $VERBOSE_PARAM
