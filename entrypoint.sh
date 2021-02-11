f99e2c6536d668914cc48091c7ca03a47f6d3343
Eli Trop <64141390+elit-cx@users.noreply.github.com>
2021-02-11 11:10:11 +0200

    Develop (#3)
    * remove kics binaries to different location
    
    * fix issue
    
    * run binaries from bin
    
    * run binaries from bin
    
    * run binaries from bin
    
    * finlize kics binary

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,14 @@ fi
 
 [[ ! -z "$INPUT_OUTPUT_PATH" ]] && OUTPUT_PATH_PARAM="-o $INPUT_OUTPUT_PATH"
 [[ ! -z "$INPUT_PAYLOAD_PATH" ]] && PAYLOAD_PATH_PARAM="-d $INPUT_PAYLOAD_PATH"
-[[ ! -z "$INPUT_QUERIES" ]] && QUERIES_PARAM="-q $INPUT_QUERIES"
 [[ ! -z "$INPUT_VERBOSE" ]] && VERBOSE_PARAM="-v"
+
+if [ ! -z "$INPUT_QUERIES" ]
+then
+    QUERIES_PARAM="-q $INPUT_QUERIES"
+else
+    QUERIES_PARAM="-q /usr/bin/assets/queries"
+fi
 
 tag=`curl --silent "https://api.github.com/repos/Checkmarx/kics/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
 echo "${DATETIME} - INF latest tag is $tag"
@@ -20,7 +26,9 @@ version=`echo $tag | sed -r 's/^.{1}//'`
 echo "${DATETIME} - INF version is $version"
 
 echo "${DATETIME} - INF downloading latest kics binaries kics_${version}_linux_x64.tar.gz"
-wget -q -c "https://github.com/Checkmarx/kics/releases/download/${tag}/kics_${version}_linux_x64.tar.gz" -O - | tar -xz &>/dev/null
+wget -q -c "https://github.com/Checkmarx/kics/releases/download/${tag}/kics_${version}_linux_x64.tar.gz" -O - | tar -xz --directory /usr/bin &>/dev/null
 
+echo "${DATETIME} - INF : current directory - ${PWD}"
 echo "${DATETIME} - INF : about to scan directory $INPUT_PATH"
-./kics $INPUT_PARAM $OUTPUT_PATH_PARAM $PAYLOAD_PATH_PARAM $QUERIES_PARAM $VERBOSE_PARAM
+echo "${DATETIME} - INF : kics command kics $INPUT_PARAM $OUTPUT_PATH_PARAM $PAYLOAD_PATH_PARAM $QUERIES_PARAM $VERBOSE_PARAM"
+kics $INPUT_PARAM $OUTPUT_PATH_PARAM $PAYLOAD_PATH_PARAM $QUERIES_PARAM $VERBOSE_PARAM
