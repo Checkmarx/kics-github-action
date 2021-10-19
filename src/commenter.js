6bdfb2b6e9bf8c9cd6af411382a5115ad04ef28e
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-10-19 10:13:18 +0100

    fixing info icon
    Signed-off-by: Rogério Peixoto <rogerio.peixoto@checkmarx.com>

--- src/commenter.js
+++ src/commenter.js
@@ -6,9 +6,10 @@ const severityIcons = {
     "HIGH": "https://user-images.githubusercontent.com/23239410/92157087-97285600-ee32-11ea-988f-0aca12c4c126.png",
     "MEDIUM": "https://user-images.githubusercontent.com/23239410/92157093-98598300-ee32-11ea-83d7-af52251a011b.png",
     "LOW": "https://user-images.githubusercontent.com/23239410/92157091-98598300-ee32-11ea-8498-19bd7d62019b.png",
-    "INFO": "https://user-images.githubusercontent.com/23239410/92157090-97c0ec80-ee32-11ea-9b2e-aa6b32b03d54.png",
-    "TRACE": "https://user-images.githubusercontent.com/23239410/92157090-97c0ec80-ee32-11ea-9b2e-aa6b32b03d54.png"
+    "INFO": "https://user-images.githubusercontent.com/75368139/137872145-b13b5200-6919-43c2-a49b-d3fdbbc20f63.png",
+    "TRACE": "https://user-images.githubusercontent.com/23239410/92157090-97c0ec80-ee32-11ea-9b2e-aa6b32b03d54.png",
 }
+const emptyIcon = "https://user-images.githubusercontent.com/75368139/137874724-5118ebc4-9769-4eb2-923d-e4ca479f747f.png"
 
 function createComment(results) {
     let message = "![kics-logo](" + kicsLogo + ")\n";
@@ -26,18 +27,18 @@ function createComment(results) {
             message += `| ![${severity}](${severityIcons[severity]}) | ${severity.toUpperCase()} | ${severityCounters[severity.toUpperCase()]} |\n`;
         }
     }
-    message += `| ![TOTAL](${severityIcons['TRACE']}) | TOTAL | ${results['total_counter']} |`;
+    message += `| ![TOTAL](${emptyIcon}) | TOTAL | ${results['total_counter']} |`;
 
     message += "\n\n</td><td>\n\n";
 
     message += "| | Metric | Values |\n";
     message += "| --- | --- | --- |\n";
-    message += `| ![placeholder](${severityIcons['TRACE']}) | Files scanned | ${results['files_scanned']}\n`;
-    message += `| ![placeholder](${severityIcons['TRACE']}) | Files parsed | ${results['files_parsed']}\n`;
-    message += `| ![placeholder](${severityIcons['TRACE']}) | Files failed to scan | ${results['files_failed_to_scan']}\n`;
-    message += `| ![placeholder](${severityIcons['TRACE']}) | Total queries | ${results['queries_total']}\n`;
-    message += `| ![placeholder](${severityIcons['TRACE']}) | Queries failed to execute | ${results['queries_failed_to_execute']}\n`;
-    message += `| ![placeholder](${severityIcons['TRACE']}) | Execution time | ${moment(results['end']).diff(moment(results['start']), 'seconds')}\n`;
+    message += `| ![placeholder](${emptyIcon}) | Files scanned | ${results['files_scanned']}\n`;
+    message += `| ![placeholder](${emptyIcon}) | Files parsed | ${results['files_parsed']}\n`;
+    message += `| ![placeholder](${emptyIcon}) | Files failed to scan | ${results['files_failed_to_scan']}\n`;
+    message += `| ![placeholder](${emptyIcon}) | Total executed queries | ${results['queries_total']}\n`;
+    message += `| ![placeholder](${emptyIcon}) | Queries failed to execute | ${results['queries_failed_to_execute']}\n`;
+    message += `| ![placeholder](${emptyIcon}) | Execution time | ${moment(results['end']).diff(moment(results['start']), 'seconds')}\n`;
 
     message += "\n</td></tr>\n</table>\n\n";
 
